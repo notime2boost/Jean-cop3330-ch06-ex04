88794a06a91fdf9fea75013299149a86bc2eb556
notime2boost <cjean176@gmail.com>
2021-11-29 15:30:47 -0500

    main

--- Jean-cop3330-ch06-ex04.cpp
+++ Jean-cop3330-ch06-ex04.cpp
@@ -1,5 +1,4 @@
-// Chapter 06, exercise 04: Name_value class with a constructor; rework exercise
-// 19 from Chapter 4 to use vector<Name_value>
+
  //*  UCF COP3330 Fall 2021 Assignment 5 Solution
  //*  Copyright 2021 Christopher Jean
 
@@ -15,8 +14,8 @@ public:
 
 int main()
 try {
-    // input
-    cout << "Enter name/value pairs, like 'Joe 17'. Terminate with NoName 0\n";
+    
+    cout << "Enter name/value pairs, like 'Chris 22'. Terminate with NoName 0\n";
     string name = "";
     int value = -1;
     vector<Name_value> nv_vec;
@@ -26,7 +25,7 @@ try {
         nv_vec.push_back(Name_value(name,value));
     }
 
-    // print list of name/value pairs
+    
     for (int i = 0; i<nv_vec.size(); ++i) {
         cout << "(" << nv_vec[i].name << ',' << nv_vec[i].value << ")\n";
     }
